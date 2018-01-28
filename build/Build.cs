1043bde645341f4d18c6831894cabf5597a96da3
Benjamin Höglinger <nefarius@dhmx.at>
2018-01-28 22:35:38 +0100

    Fixed build configuration

--- build/Build.cs
+++ build/Build.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Linq;
-using Nuke.Common;
-using Nuke.Common.Git;
-using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.MSBuild;
+﻿using Nuke.Common.Tools.MSBuild;
 using Nuke.Core;
+using Nuke.Core.BuildServers;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 using static Nuke.Core.IO.FileSystemTasks;
 using static Nuke.Core.IO.PathConstruction;
-using static Nuke.Core.EnvironmentInfo;
 
 class Build : NukeBuild
 {
-    // Console application entry. Also defines the default target.
-    public static int Main () => Execute<Build>(x => x.Compile);
-
     // Auto-injection fields:
 
     // [GitVersion] readonly GitVersion GitVersion;
@@ -27,24 +19,33 @@ class Build : NukeBuild
     // Returns command-line arguments and environment variables.
 
     Target Clean => _ => _
-            .OnlyWhen(() => false) // Disabled for safety.
-            .Executes(() =>
-            {
-                DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
-                EnsureCleanDirectory(OutputDirectory);
-            });
+        .OnlyWhen(() => false) // Disabled for safety.
+        .Executes(() =>
+        {
+            DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
+            EnsureCleanDirectory(OutputDirectory);
+        });
 
     Target Restore => _ => _
-            .DependsOn(Clean)
-            .Executes(() =>
-            {
-                MSBuild(s => DefaultMSBuildRestore);
-            });
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            MSBuild(s => DefaultMSBuildRestore);
+        });
 
     Target Compile => _ => _
-            .DependsOn(Restore)
-            .Executes(() =>
-            {
-                MSBuild(s => DefaultMSBuildCompile);
-            });
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            MSBuild(s => DefaultMSBuildCompile
+                .SetAssemblyVersion(AppVeyor.Instance.BuildVersion)
+                .SetFileVersion(AppVeyor.Instance.BuildVersion)
+                .SetInformationalVersion(AppVeyor.Instance.BuildVersion));
+        });
+
+    // Console application entry. Also defines the default target.
+    public static int Main()
+    {
+        return Execute<Build>(x => x.Compile);
+    }
 }
