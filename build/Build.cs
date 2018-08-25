f3ecc1100fa25fb8f9167cabc6db24a6d5e638db
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-08-25 22:52:23 +0200

    Updated to latest nuke build

--- build/Build.cs
+++ build/Build.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.BuildServers;
 using Nuke.Common.Git;
-using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.MSBuild;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -12,46 +11,49 @@ using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
 class Build : NukeBuild
 {
-    // Console application entry point. Also defines the default target.
     public static int Main () => Execute<Build>(x => x.Compile);
 
-    // Auto-injection fields:
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
 
-    // [GitVersion] readonly GitVersion GitVersion;
-    // Semantic versioning. Must have 'GitVersion.CommandLine' referenced.
-
-    // [GitRepository] readonly GitRepository GitRepository;
-    // Parses origin, branch name and head from git config.
-
-    // [Parameter] readonly string MyGetApiKey;
-    // Returns command-line arguments and environment variables.
-    
-    // [Solution] readonly Solution Solution;
-    // Provides access to the structure of the solution.
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
     Target Clean => _ => _
-            .OnlyWhen(() => false) // Disabled for safety.
-            .Executes(() =>
-            {
-                DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
-                EnsureCleanDirectory(OutputDirectory);
-            });
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
 
     Target Restore => _ => _
-            .DependsOn(Clean)
-            .Executes(() =>
-            {
-                MSBuild(s => DefaultMSBuildRestore);
-            });
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            MSBuild(s => s
+                .SetTargetPath(SolutionFile)
+                .SetTargets("Restore"));
+        });
 
     Target Compile => _ => _
-            .DependsOn(Restore)
-            .Executes(() =>
-            {
-                MSBuild(s => DefaultMSBuildCompile
-                    .SetAssemblyVersion(AppVeyor.Instance.BuildVersion)
-                    .SetFileVersion(AppVeyor.Instance.BuildVersion)
-                    .SetInformationalVersion(AppVeyor.Instance.BuildVersion)
-                    .SetPackageVersion(AppVeyor.Instance.BuildVersion));
-            });
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            MSBuild(s => s
+                .SetTargetPath(SolutionFile)
+                .SetTargets("Rebuild")
+                .SetConfiguration(Configuration)
+                .SetMaxCpuCount(Environment.ProcessorCount)
+                .SetNodeReuse(IsLocalBuild));
+        });
+
+    private Target Pack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            MSBuild(s => s
+                .SetTargetPath(SolutionFile)
+                .SetTargets("Restore", "Pack")
+                .SetPackageOutputPath(ArtifactsDirectory)
+                .SetConfiguration(Configuration)
+                .EnableIncludeSymbols());
+        });
 }
