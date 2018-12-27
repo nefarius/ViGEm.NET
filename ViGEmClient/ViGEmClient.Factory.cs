1adc0528bfe33ac142cd3be4c97016a0bb0b434e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-12-27 17:11:29 +0100

    Added comments

--- ViGEmClient/ViGEmClient.Factory.cs
+++ ViGEmClient/ViGEmClient.Factory.cs
@@ -4,21 +4,41 @@ namespace Nefarius.ViGEm.Client
 {
     public partial class ViGEmClient
     {
+        /// <summary>
+        ///     Allocates a new virtual Xbox 360 Controller device on the bus.
+        /// </summary>
+        /// <returns>A new virtual Xbox 360 Controller device.</returns>
         public IVirtualGamepad CreateXbox360Controller()
         {
             return new Xbox360Controller(this);
         }
 
+        /// <summary>
+        ///     Allocates a new virtual Xbox 360 Controller device on the bus with custom vendor and product identifiers.
+        /// </summary>
+        /// <param name="vendorId">16-bit unsigned vendor ID.</param>
+        /// <param name="productId">16-bit unsigned product ID.</param>
+        /// <returns>A new virtual Xbox 360 Controller device.</returns>
         public IVirtualGamepad CreateXbox360Controller(ushort vendorId, ushort productId)
         {
             return new Xbox360Controller(this, vendorId, productId);
         }
 
+        /// <summary>
+        ///     Allocates a new virtual DualShock 4 device on the bus.
+        /// </summary>
+        /// <returns>A new virtual DualShock 4 Controller device.</returns>
         public IVirtualGamepad CreateDualShock4Controller()
         {
             return new DualShock4Controller(this);
         }
 
+        /// <summary>
+        ///     Allocates a new virtual DualShock 4 device on the bus.
+        /// </summary>
+        /// <param name="vendorId">16-bit unsigned vendor ID.</param>
+        /// <param name="productId">16-bit unsigned product ID.</param>
+        /// <returns>A new virtual DualShock 4 Controller device.</returns>
         public IVirtualGamepad CreateDualShock4Controller(ushort vendorId, ushort productId)
         {
             return new DualShock4Controller(this, vendorId, productId);
