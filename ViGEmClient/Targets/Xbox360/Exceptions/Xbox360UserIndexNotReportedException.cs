ae6fac3b5ac9e30d8fcae61f4acad88f8e6d3d64
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-03-27 15:22:34 +0100

    Added Xbox360UserIndexNotReportedException

---
+++ ViGEmClient/Targets/Xbox360/Exceptions/Xbox360UserIndexNotReportedException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Nefarius.ViGEm.Client.Targets.Xbox360.Exceptions
+{
+    [UsedImplicitly]
+    public class Xbox360UserIndexNotReportedException : Exception
+    {
+    }
+}
