ef9bfbc33206e05bd9085608b2d11ee133c43dd4
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-08-27 17:16:13 -0400

    Added DualShock4DPadDirection

--- ViGEmClient/Targets/DualShock4/DualShock4Properties.cs
+++ ViGEmClient/Targets/DualShock4/DualShock4Properties.cs
@@ -182,6 +182,91 @@ namespace Nefarius.ViGEm.Client.Targets.DualShock4
         }
     }
 
+    public abstract class DualShock4DPadDirection : DualShock4Property
+    {
+        public static DualShock4DPadDirection None = new NoneDPadDirection();
+        public static DualShock4DPadDirection Northwest = new NorthwestDPadDirection();
+        public static DualShock4DPadDirection West = new WestDPadDirection();
+        public static DualShock4DPadDirection Southwest = new SouthwestDPadDirection();
+        public static DualShock4DPadDirection South = new SouthDPadDirection();
+        public static DualShock4DPadDirection Southeast = new SoutheastDPadDirection();
+        public static DualShock4DPadDirection East = new EastDPadDirection();
+        public static DualShock4DPadDirection Northeast = new NortheastDPadDirection();
+        public static DualShock4DPadDirection North = new NorthDPadDirection();
+
+        protected DualShock4DPadDirection(int id, string name,ushort value)
+            : base(id, name)
+        {
+            Value = value;
+        }
+
+        [IgnoreDataMember]
+        public ushort Value { get; }
+
+        private class NoneDPadDirection : DualShock4DPadDirection
+        {
+            public NoneDPadDirection() : base(0, "None", 0x8)
+            {
+            }
+        }
+
+        private class NorthwestDPadDirection : DualShock4DPadDirection
+        {
+            public NorthwestDPadDirection() : base(1, "Northwest", 0x7)
+            {
+            }
+        }
+
+        private class WestDPadDirection : DualShock4DPadDirection
+        {
+            public WestDPadDirection() : base(2, "West", 0x6)
+            {
+            }
+        }
+
+        private class SouthwestDPadDirection : DualShock4DPadDirection
+        {
+            public SouthwestDPadDirection() : base(3, "Southwest", 0x5)
+            {
+            }
+        }
+
+        private class SouthDPadDirection : DualShock4DPadDirection
+        {
+            public SouthDPadDirection() : base(4, "South", 0x4)
+            {
+            }
+        }
+
+        private class SoutheastDPadDirection : DualShock4DPadDirection
+        {
+            public SoutheastDPadDirection() : base(5, "Southeast", 0x3)
+            {
+            }
+        }
+
+        private class EastDPadDirection : DualShock4DPadDirection
+        {
+            public EastDPadDirection() : base(6, "East", 0x2)
+            {
+            }
+        }
+
+        private class NortheastDPadDirection : DualShock4DPadDirection
+        {
+            public NortheastDPadDirection() : base(7, "Northeast", 0x1)
+            {
+            }
+        }
+
+        private class NorthDPadDirection : DualShock4DPadDirection
+        {
+            public NorthDPadDirection() : base(8, "North", 0x0)
+            {
+            }
+        }
+    }
+
     public abstract class DualShock4Axis : DualShock4Property
     {
         public static DualShock4Axis LeftThumbX = new LeftThumbXAxis();
