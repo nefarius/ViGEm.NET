523b2cd2f5bedc08fe6b3eb34b689a09d13a8b46
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-08-28 08:33:55 -0400

    Added IDualShock4Controller

--- ViGEmClient/Targets/IDualShock4Controller.cs
+++ ViGEmClient/Targets/IDualShock4Controller.cs
@@ -1,7 +1,20 @@
-﻿namespace Nefarius.ViGEm.Client.Targets
+﻿using JetBrains.Annotations;
+using Nefarius.ViGEm.Client.Targets.DualShock4;
+
+namespace Nefarius.ViGEm.Client.Targets
 {
     public interface IDualShock4Controller : IVirtualGamepad
     {
-        
+        [UsedImplicitly]
+        void SetButtonState(DualShock4Button button, bool pressed);
+
+        [UsedImplicitly]
+        void SetAxisValue(DualShock4Axis axis, short value);
+
+        [UsedImplicitly]
+        void SetSliderValue(DualShock4Slider slider, byte value);
+
+        [UsedImplicitly]
+        event DualShock4FeedbackReceivedEventHandler FeedbackReceived;
     }
 }
