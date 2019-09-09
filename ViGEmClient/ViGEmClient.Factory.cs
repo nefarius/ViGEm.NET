24a71ef9acae0c2162a1a51d630c1a190158f357
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-09-09 14:40:17 +0200

    Fixed factory method for DualShock 4 Controller

--- ViGEmClient/ViGEmClient.Factory.cs
+++ ViGEmClient/ViGEmClient.Factory.cs
@@ -32,7 +32,7 @@ namespace Nefarius.ViGEm.Client
         /// </summary>
         /// <returns>A new virtual DualShock 4 Controller device.</returns>
         [UsedImplicitly]
-        public IVirtualGamepad CreateDualShock4Controller()
+        public IDualShock4Controller CreateDualShock4Controller()
         {
             return new DualShock4Controller(this);
         }
@@ -44,7 +44,7 @@ namespace Nefarius.ViGEm.Client
         /// <param name="productId">16-bit unsigned product ID.</param>
         /// <returns>A new virtual DualShock 4 Controller device.</returns>
         [UsedImplicitly]
-        public IVirtualGamepad CreateDualShock4Controller(ushort vendorId, ushort productId)
+        public IDualShock4Controller CreateDualShock4Controller(ushort vendorId, ushort productId)
         {
             return new DualShock4Controller(this, vendorId, productId);
         }
