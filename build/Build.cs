142d28c1026d4f28c3845927a27f78459c45296e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-08 22:02:13 +0100

    Updated to latest nuke Adapted appveyor.yml to use cmd wrapper

--- build/Build.cs
+++ build/Build.cs
@@ -1,29 +1,46 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using Nuke.Common;
+using Nuke.Common.CI;
 using Nuke.Common.CI.AppVeyor;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
-internal class Build : NukeBuild
+[CheckBuildProjectConfigurations]
+class Build : NukeBuild
 {
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    private readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution("ViGEm.NET.sln")] private readonly Solution Solution;
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
 
-    private AbsolutePath ArtifactsDirectory => RootDirectory / "bin";
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
 
-    private Target Clean => _ => _
-        .Executes(() => { EnsureCleanDirectory(ArtifactsDirectory); });
-
-    private Target Restore => _ => _
-        .DependsOn(Clean)
+    Target Restore => _ => _
         .Executes(() =>
         {
             MSBuild(s => s
@@ -31,7 +48,7 @@ internal class Build : NukeBuild
                 .SetTargets("Restore"));
         });
 
-    private Target Compile => _ => _
+    Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
@@ -74,20 +91,4 @@ internal class Build : NukeBuild
                 .SetPackageVersion(AppVeyor.Instance?.BuildVersion));
         });
 
-    private Target Pack => _ => _
-        .DependsOn(Compile)
-        .Executes(() =>
-        {
-            MSBuild(s => s
-                .SetTargetPath(Solution)
-                .SetTargets("Restore", "Pack")
-                .SetPackageOutputPath(ArtifactsDirectory)
-                .SetConfiguration(Configuration)
-                .EnableIncludeSymbols());
-        });
-
-    public static int Main()
-    {
-        return Execute<Build>(x => x.Compile);
-    }
 }
