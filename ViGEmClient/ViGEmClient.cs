2e75329a1769919bdc2d658bb251a66b9c323b21
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-02-04 20:33:19 +0100

    Fixed IDisposable pattern on ViGEmClient class

--- ViGEmClient/ViGEmClient.cs
+++ ViGEmClient/ViGEmClient.cs
@@ -1,80 +1,62 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
 using Nefarius.ViGEm.Client.Exceptions;
 
-namespace Nefarius.ViGEm.Client
+namespace Nefarius.ViGEm.Client;
+
+using PVIGEM_CLIENT = IntPtr;
+
+/// <summary>
+///     Represents a managed gateway to a compatible emulation bus.
+/// </summary>
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public sealed partial class ViGEmClient : IDisposable
 {
-    using PVIGEM_CLIENT = IntPtr;
+    [SuppressMessage("ReSharper", "SwitchStatementMissingSomeEnumCasesNoDefault")]
+    public ViGEmClient()
+    {
+        NativeHandle = vigem_alloc();
+
+        if (NativeHandle == IntPtr.Zero)
+        {
+            throw new VigemAllocFailedException();
+        }
+
+        VIGEM_ERROR error = vigem_connect(NativeHandle);
+
+        switch (error)
+        {
+            case VIGEM_ERROR.VIGEM_ERROR_ALREADY_CONNECTED:
+                throw new VigemAlreadyConnectedException();
+            case VIGEM_ERROR.VIGEM_ERROR_BUS_NOT_FOUND:
+                throw new VigemBusNotFoundException();
+            case VIGEM_ERROR.VIGEM_ERROR_BUS_ACCESS_FAILED:
+                throw new VigemBusAccessFailedException();
+            case VIGEM_ERROR.VIGEM_ERROR_BUS_VERSION_MISMATCH:
+                throw new VigemBusVersionMismatchException();
+        }
+    }
 
     /// <summary>
-    ///     Represents a managed gateway to a compatible emulation bus.
+    ///     Gets the <see cref="PVIGEM_CLIENT" /> identifying the bus connection.
     /// </summary>
-    public partial class ViGEmClient : IDisposable
+    internal PVIGEM_CLIENT NativeHandle { get; }
+
+    public void Dispose()
     {
-        public ViGEmClient()
-        {
-            NativeHandle = vigem_alloc();
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+    
+    private void ReleaseUnmanagedResources()
+    {
+        vigem_disconnect(NativeHandle);
+        vigem_free(NativeHandle);
+    }
 
-            if (NativeHandle == IntPtr.Zero)
-                throw new VigemAllocFailedException();
-
-            var error = vigem_connect(NativeHandle);
-
-            switch (error)
-            {
-                case VIGEM_ERROR.VIGEM_ERROR_ALREADY_CONNECTED:
-                    throw new VigemAlreadyConnectedException();
-                case VIGEM_ERROR.VIGEM_ERROR_BUS_NOT_FOUND:
-                    throw new VigemBusNotFoundException();
-                case VIGEM_ERROR.VIGEM_ERROR_BUS_ACCESS_FAILED:
-                    throw new VigemBusAccessFailedException();
-                case VIGEM_ERROR.VIGEM_ERROR_BUS_VERSION_MISMATCH:
-                    throw new VigemBusVersionMismatchException();
-            }
-        }
-
-        /// <summary>
-        ///     Gets the <see cref="PVIGEM_CLIENT"/> identifying the bus connection.
-        /// </summary>
-        internal PVIGEM_CLIENT NativeHandle { get; }
-
-        #region IDisposable Support
-
-        private bool disposedValue; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-                vigem_disconnect(NativeHandle);
-                vigem_free(NativeHandle);
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        ~ViGEmClient()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(false);
-        }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            GC.SuppressFinalize(this);
-        }
-
-        #endregion
+    ~ViGEmClient()
+    {
+        ReleaseUnmanagedResources();
     }
 }
